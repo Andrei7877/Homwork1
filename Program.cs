2a5d9ca3514019ab50be65b04f95de385cb396ac
login <107126357+Iandrei789@users.noreply.github.com>
2022-08-10 14:13:17 +0200

    поочередно заполняем строки

--- Program.cs
+++ Program.cs
@@ -1,3 +1,8 @@
 ﻿Console.Write("Введите количество строк, которые хотите ввести: ");
 int n = Convert.ToInt32(Console.ReadLine());      
 string[] strs = new string[n];           
+for (int i = 0; i < n; i++)
+{
+Console.Write("Введите строку №{0}:\r\n    ", i + 1);
+strs[i] = Console.ReadLine();       
+}
